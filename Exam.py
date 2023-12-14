dd3ce21f7ab104ee3a7dbfe500a1f24a516bf8fe
Sia <122100161+Siaang@users.noreply.github.com>
2023-12-14 19:42:19 +0800

    Update Exam.py

--- Exam.py
+++ Exam.py
@@ -1,22 +1,19 @@
-from bigtree import Node
 from collections import defaultdict, deque
+from bigtree import Node
 
 path_dict = {
     "1": {"name": "1"},
     "1/2": {"name": "2"},
     "1/3": {"name": "3"},
-
     "1/2/4": {"name": "4"},
     "1/2/5": {"name": "5"},
-
     "1/3/6": {"name": "6"},
     "1/3/7": {"name": "7"},
-
-    "1/6/8": {"name": "8"},
-    "1/7/9": {"name": "9"},
+    "1/3/6/8": {"name": "8"},
+    "1/3/7/9": {"name": "9"},
 }
 
-created_nodes = {"1": Node("1", data = path_dict["1"] )}
+created_nodes = {"1": Node("1", data=path_dict["1"])}
 
 for path, node_data in path_dict.items():
     if path != "1":
@@ -24,16 +21,18 @@ for path, node_data in path_dict.items():
         current_node = created_nodes[nodes[0]]
         for node in nodes[1:]:
             if node not in created_nodes:
-                created_nodes[node] = Node(node_data["name"], parent = current_node, data = path_dict[path])
+                created_nodes[node] = Node(node_data["name"], parent=current_node, data=path_dict[path])
             current_node = created_nodes[node]
 
-created_nodes["1"].show(attr_list = ["name"])
+created_nodes["1"].show(attr_list=["name"])
+
 
 class Nodes:
     def __init__(self, current):
         self.current = current
         self.right = None
         self.left = None
+
 
 def create_tree():
     root = Nodes(1)
@@ -46,6 +45,7 @@ def create_tree():
     root.right.left.right = Nodes(8)
     root.right.right.right = Nodes(9)
     return root
+
 
 def vertical_traversal(root):
     if not root:
@@ -68,16 +68,12 @@ def vertical_traversal(root):
 
     result = []
     for distance in sorted_dist:
-        result.extend(vertical_order[distance])
+        result.extend(sorted(vertical_order[distance], reverse=True))
 
     return result
 
+
 root = create_tree()
 result = vertical_traversal(root)
-print("="*30 + "\nOutput: ")
+print("=" * 30 + "\nOutput: ")
 print(result)
-
-
-
-
-
